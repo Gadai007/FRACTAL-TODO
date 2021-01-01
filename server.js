211c869820d94fa20591f2285ea3260a90eebf54
Your Name <you@example.com>
2021-01-01 18:56:28 +0530

    boilerplate code ready

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
+require('dotenv').config()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+
+mongoose.connect(process.env.MONGO_URI, 
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`server connected on ${PORT}`)
+        })
+    })
+
+
