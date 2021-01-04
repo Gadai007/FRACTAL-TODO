0000a2049c326dab7507245e44fafa7fe8446818
Your Name <you@example.com>
2021-01-04 14:15:07 +0530

    project ready for deployment

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer mt-auto py-3 bg-primary">
-            <div className="container-fluid text-center">
-                <span className='text-light' >Project submitted by Debayan Debnath</span>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer mt-auto py-3 bg-primary">
+      <div className="container-fluid text-center">
+        <span className="text-light">Project submitted by Debayan Debnath</span>
+        <a href="https://www.linkedin.com/in/debayan1221/">
+          <i className="fab fa-linkedin text-light ps-3"></i>
+        </a>
+        <a href="https://github.com/Gadai007/">
+          <i className="fab fa-github text-light ps-3"></i>
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- server.js
+++ server.js
@@ -22,6 +22,10 @@ mongoose.connect(process.env.MONGO_URI,
         })
     })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
 app.use('/api', authRoute)
 app.use('/api', userRoute)
 app.use('/api', bucketRoute)
